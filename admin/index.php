2f71ef1e20671772f3abd18578c9e8966b60897b
pagol9691-oss <pagol9691@gmail.com>
2026-09-18 00:10:44 +0300

    Update bootstrap path in index.php

--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,7 @@
 <?php
 
 declare(strict_types=1);
-require_once __DIR__ . '/includes/bootstrap.php';
+require_once __DIR__ . '/../includes/bootstrap.php';
 requireLogin('../login.php');
 requireRole('super_admin', '../login.php');
 
